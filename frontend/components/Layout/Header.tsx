565e9e52527f55dce952cd7ac66870a7946c1c9b
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2023-01-18 15:39:15 +0100

    WIP

--- frontend/components/Layout/Header.tsx
+++ frontend/components/Layout/Header.tsx
@@ -1,11 +1,30 @@
 export default function Header(): JSX.Element {
   return (
-    <div className="bg-svw-blue-default ">
-      <div className="container">
-        <div>logo</div>
-        <div>
-          <div>cta</div>
-          <div>nav items</div>
+    <>
+      <SmallScreenNavigation />
+      <LargeScreenNavigation />
+    </>
+  );
+}
+
+function SmallScreenNavigation(): JSX.Element {
+  return <div className="md:hidden">not yet implemented</div>;
+}
+
+function LargeScreenNavigation(): JSX.Element {
+  return (
+    <div className="hidden md:block bg-svw-blue-default">
+      <div className="container flex flex-row">
+        <div className="flex-none">logo</div>
+        <div className="flex flex-col flex-1">
+          <div className="flex flex-row place-content-end">
+            <a>cta 1</a>
+            <a>cta 2</a>
+          </div>
+          <div className="flex flex-row place-content-end">
+            <a>nav items 1</a>
+            <a>nav items 1</a>
+          </div>
         </div>
       </div>
     </div>
