51bd2351469899f9aa44966c8bb0d2c6fd76c1e9
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2023-01-26 20:34:41 +0100

    Remove rounded, add top border

--- frontend/components/person/PersonCard.tsx
+++ frontend/components/person/PersonCard.tsx
@@ -10,16 +10,16 @@ export function PersonCard({ person }: Props): JSX.Element {
   const picture = person.picture?.data?.attributes;
   return (
     <div className="mt-16 flex justify-center">
-      <div className="flex flex-col md:flex-row md:max-w-xl rounded-lg bg-white shadow-lg">
+      <div className="flex flex-col md:flex-row bg-white shadow-lg border-t-4 border-svw-blue-default">
         <Image
-          className="w-full h-96 md:h-auto object-cover md:w-48 rounded-t-lg md:rounded-none md:rounded-l-lg"
+          className="w-full h-96 md:h-auto object-cover md:w-48"
           src={getFullAssetUrl(picture?.url ?? "")}
           alt=""
           width={picture?.width ?? 0}
           height={picture?.height ?? 0}
         />
         <div className="w-96 p-6 flex flex-col justify-start">
-          <h5 className="text-gray-900 text-xl font-medium mb-2">
+          <h5 className="mb-2">
             {person.firstname} {person.lastname}
           </h5>
           <p className="text-gray-700 text-base mb-4">
