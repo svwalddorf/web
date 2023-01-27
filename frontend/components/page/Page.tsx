b6bd8da62d0ba80bba9e88b9366171aa619e71c3
Matthäus Mayer <7984982+theMattCode@users.noreply.github.com>
2023-01-27 08:13:12 +0100

    Clean up

--- frontend/components/page/Page.tsx
+++ frontend/components/page/Page.tsx
@@ -9,51 +9,38 @@ export function Page(props: Props): JSX.Element {
   const { mainContents, leftContents, rightContents, headerContents } = props.data;
   return (
     <>
-      {/*showSubPageNavigation && (
-        <div className="mt-8  border-b border-neutral-300">
-          <div className="container flex place-content-end pl-4">
-            {subPages?.map((subPage) => (
-              <div key={subPage.attributes?.slug} className="px-3">
-                {subPage.attributes?.title}
-              </div>
-            ))}
-          </div>
-        </div>
-      )*/}
-      <div>
-        <div className="flex flex-row">
-          {headerContents?.map((content) => {
+      <div className="flex flex-row">
+        {headerContents?.map((content) => {
+          if (!content || content.__typename === "Error") {
+            return null;
+          }
+          return <DynamicContent key={content.id} component={content} />;
+        })}
+      </div>
+      <div className="container flex flex-row place-content-between">
+        <div className="">
+          {leftContents?.map((content) => {
             if (!content || content.__typename === "Error") {
               return null;
             }
             return <DynamicContent key={content.id} component={content} />;
           })}
         </div>
-        <div className="container flex flex-row place-content-between">
-          <div className="">
-            {leftContents?.map((content) => {
-              if (!content || content.__typename === "Error") {
-                return null;
-              }
-              return <DynamicContent key={content.id} component={content} />;
-            })}
-          </div>
-          <main className="">
-            {mainContents?.map((content) => {
-              if (!content || content.__typename === "Error") {
-                return null;
-              }
-              return <DynamicContent key={content.id} component={content} />;
-            })}
-          </main>
-          <div className="">
-            {rightContents?.map((content) => {
-              if (!content || content.__typename === "Error") {
-                return null;
-              }
-              return <DynamicContent key={content.id} component={content} />;
-            })}
-          </div>
+        <main className="">
+          {mainContents?.map((content) => {
+            if (!content || content.__typename === "Error") {
+              return null;
+            }
+            return <DynamicContent key={content.id} component={content} />;
+          })}
+        </main>
+        <div className="">
+          {rightContents?.map((content) => {
+            if (!content || content.__typename === "Error") {
+              return null;
+            }
+            return <DynamicContent key={content.id} component={content} />;
+          })}
         </div>
       </div>
     </>
